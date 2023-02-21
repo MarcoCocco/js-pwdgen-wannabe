02955616ba5e491b0e8f2f51a979eef23ac37d98
MarcoCocco <marcococco28@gmail.com>
2023-02-21 14:28:24 +0100

    feat: aggiunto lo script con i prompt e alert finale fine esercizio

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let userName = prompt('Dimmi il tuo nome');
+console.log(`Piacere ${userName}`);
+
+let userSurname = prompt('Il tuo cognome?');
+console.log(`Ottimo signor ${userSurname}`);
+
+let favoriteColor = prompt('Qual è il tuo colore preferito?');
+console.log(`Bene, è proprio un bel colore il ${favoriteColor}`);
+
+let numVal = 77;
+console.log(numVal);
+
+alert(`Ricapitolando, tu sei ${userSurname} ${userName} e il tuo colore preferito è il ${favoriteColor}. Alla luce di questo ti abbiamo assegnato il seguente valore: ${numVal}`);
